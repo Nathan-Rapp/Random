e696de03dd0c3f33cbc2d03978f70abd12a73340
Nathan-Rapp <122110572+Nathan-Rapp@users.noreply.github.com>
2023-01-06 11:25:48 -0500

    Delete Install.cmd

--- Install.cmd
+++
@@ -1,7 +0,0 @@
-echo -text "Initilize Random"
-getkey -id "rand" -strvar $ANSWER -errlabel END
-echo -text "Random Installation"
-setenv -name RAND -value $BOOTPATH
-config -filename $BOOTPATH/SYS.cfg -domain SYS -internal
-echo -text "Random Done."
-#END
